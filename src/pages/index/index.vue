1d27b0e4055f88cc5ab421393ddf2b9419b27fb0
alpen <3311432144@qq.com>
2024-09-11 23:09:31 +0800

    主页面框架编写

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,51 +1,22 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
+  <view>
+     <view class="flex items-center h-[100rpx] bg-gray-200  pos-fixed left-0 w-[100%]">
+      <view class=" flex flex-col justify-center items-center w-[50rpx] text-xl h-[50rpx] ">
+        <text class="text-30rpx">09</text>
+        <text class="text-30rpx">月</text>
+      </view>
+     <view v-for="index in 7" class="w-[calc((100%-50rpx)/7)] h-30% flex flex-col justify-center items-center">
+      <text class="text-35rpx text-black">周{{ index }}</text>
+      <text class="text-24rpx text-coolGray">{{ index }}日</text>
+     </view>
     </view>
-    <div class="m-1 text-red-500">
-      UnocssDemo
-    </div>
+  </view>
+  <view class="flex flex-col justify-center w-[100%] pt-[50px]">
+    <view class="w-[50rpx] flex flex-col justify-center items-center">
+      <view class=" h-150rpx flex flex-col justify-center items-center w-[100%] " v-for="index in 20" key="index">
+        {{ index>9?index:'0'+(index) }}
+      </view>
+    </view>
   </view>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      title: 'Hello',
-    }
-  },
-  onLoad() {},
-  methods: {},
-}
-</script>
-
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
+ 
--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,12 @@
 // uno.config.ts，支持 HBuilderX
 import { defineConfig } from 'unocss'
 import { presetUni } from '@uni-helper/unocss-preset-uni'
+import { presetRemRpx } from '@unocss-applet/preset-rem-rpx'
 
 export default defineConfig({
   presets: [
-    presetUni()
+    presetUni(),
+     //将单位转为rpx
+     presetRemRpx({ baseFontSize: 16, screenWidth: 375, mode: 'rem2rpx' }),
   ]
 })
